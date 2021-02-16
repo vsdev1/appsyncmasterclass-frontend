f004de075f7ad73ba3304fbbf44304deccadeca8
Volker Sommerfeld <volker.sommerfeld@idealo.de>
2021-02-16 18:36:32 +0100

    use environment variables starting with VUE_APP_

--- src/main.js
+++ src/main.js
@@ -6,16 +6,16 @@ import '@aws-amplify/ui-vue'
 
 Amplify.configure({
   Auth: {
-    region: process.env.AWS_REGION,
-    userPoolId: process.env.USER_POOL_ID,
-    userPoolWebClientId: process.env.WEB_COGNITO_USER_POOL_CLIENT_ID,
+    region: process.env.VUE_APP_AWS_REGION,
+    userPoolId: process.env.VUE_APP_USER_POOL_ID,
+    userPoolWebClientId: process.env.VUE_APP_WEB_COGNITO_USER_POOL_CLIENT_ID,
     mandatorySignIn: true
   }
 })
 
 const myAppConfig = {
-  'aws_appsync_graphqlEndpoint': process.env.AWS_APPSYNC_GRAPHQL_ENDPOINT,
-  'aws_appsync_region': process.env.AWS_REGION,
+  'aws_appsync_graphqlEndpoint': process.env.VUE_APP_AWS_APPSYNC_GRAPHQL_ENDPOINT,
+  'aws_appsync_region': process.env.VUE_APP_AWS_REGION,
   'aws_appsync_authenticationType': 'AMAZON_COGNITO_USER_POOLS'
 }
 
